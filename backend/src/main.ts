342e96c8d5b63519338161e6c92cf15423f912f7
nextunicorn2026 <nextunicorn2026@gmail.com>
2026-07-11 05:43:04 +0530

    feat: set global API prefix for backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,6 +11,7 @@ import { KafkaService } from './common/kafka/kafka.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors();
 
